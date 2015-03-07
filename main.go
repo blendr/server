e209ed22e0d4dbfc2dbda087c7401b8436aee76f
Nate Brennand <natebrennand@gmail.com>
2015-03-07 17:53:14 -0500

    start handling oauth credentials

--- main.go
+++ main.go
@@ -3,17 +3,11 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 
-	"code.google.com/p/goauth2/oauth"
-	"code.google.com/p/google-api-go-client/plus/v1"
 	"github.com/google/google-api-go-client/gmail/v1"
 	"github.com/gorilla/sessions"
 )
@@ -26,7 +20,9 @@ type appError struct {
 
 const (
 	applicationName = "Gmail Peer Edit"
-	sessionKey      = "gmail-code"
+
+	sessionKey = "blendr"
+	codeKey    = "gmail-code"
 )
 
 var (
@@ -36,8 +32,6 @@ var (
 
 	// store initializes the Gorilla session store.
 	store = sessions.NewCookieStore([]byte("qwerty123")) // TODO: configure
-
-	oauthClient *http.Client
 )
 
 func init() {
@@ -45,118 +39,24 @@ func init() {
 	if serverPort == "" {
 		log.Fatal("No value found in environment for PORT")
 	}
-
-	/*
-		// Your credentials should be obtained from the Google
-		// Developer Console (https://console.developers.google.com).
-		conf := &oauth2.Config{
-			ClientID:     clientID,
-			ClientSecret: clientSecret,
-			RedirectURL:  "http://localhost:5000/oauth2callback",
-			Scopes: []string{
-				gmail.MailGoogleComScope,
-			},
-			Endpoint: google.Endpoint,
-		}
-		// Redirect user to Google's consent page to ask for permission
-		// for the scopes specified above.
-		url := conf.AuthCodeURL("state")
-		fmt.Printf("Visit the URL for the auth dialog: %v\n", url)
-
-		// Handle the exchange code to initiate a transport.
-		tok, err := conf.Exchange(oauth2.NoContext, "authorization-code")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		oauthClient = conf.Client(oauth2.NoContext, tok)
-	*/
 }
 
-// indexTemplate is the HTML template we use to present the index page.
-var indexTemplate = template.Must(template.ParseFiles("index.html"))
+func checkIfAuthenticated(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, sessionKey)
+		_, exists := session.Values[codeKey]
 
-// index sets up a session for the current user and serves the index page
-func index(w http.ResponseWriter, r *http.Request) {
-	// This check prevents the "/" handler from handling all requests by default
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Create a state token to prevent request forgery and store it in the session
-	// for later validation
-	session, err := store.Get(r, "sessionName")
-	if err != nil {
-		log.Println("error fetching session:", err)
-		// Ignore the initial session fetch error, as Get() always returns a
-		// session, even if empty.
-		//return &appError{err, "Error fetching session", 500}
-	}
-	state := randomString(64)
-	session.Values["state"] = state
-	session.Save(r, w)
-
-	stateURL := url.QueryEscape(session.Values["state"].(string))
-
-	// Fill in the missing fields in index.html
-	var data = struct {
-		ApplicationName, ClientID, State string
-	}{applicationName, clientID, stateURL}
-
-	// Render and serve the HTML
-	err = indexTemplate.Execute(w, data)
-	if err != nil {
-		log.Println("error rendering template:", err)
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "error rendering template => {%s}", err)
-	}
-}
-
-// people fetches the list of people user has shared with this app
-func people(w http.ResponseWriter, r *http.Request) *appError {
-	session, err := store.Get(r, "sessionName")
-	if err != nil {
-		log.Println("error fetching session:", err)
-		return &appError{err, "Error fetching session", 500}
-	}
-	token := session.Values["accessToken"]
-	// Only fetch a list of people for connected users
-	if token == nil {
-		m := "Current user not connected"
-		return &appError{errors.New(m), m, 401}
-	}
-
-	// Create a new authorized API client
-	t := &oauth.Transport{Config: oauthCfg}
-	tok := new(oauth.Token)
-	tok.AccessToken = token.(string)
-	t.Token = tok
-	service, err := plus.New(t.Client())
-	if err != nil {
-		return &appError{err, "Create Plus Client", 500}
-	}
-
-	// Get a list of people that this user has shared with this app
-	people := service.People.List("me", "visible")
-	peopleFeed, err := people.Do()
-	if err != nil {
-		m := "Failed to refresh access token"
-		if err.Error() == "AccessTokenRefreshError" {
-			return &appError{errors.New(m), m, 500}
+		if !exists {
+			http.Redirect(w, r, "/authenticate", http.StatusSeeOther)
+			return
 		}
-		return &appError{err, m, 500}
-	}
-	w.Header().Set("Content-type", "application/json")
-	err = json.NewEncoder(w).Encode(&peopleFeed)
-	if err != nil {
-		return &appError{err, "Convert PeopleFeed to JSON", 500}
-	}
-	return nil
+
+		h.ServeHTTP(w, r)
+	})
 }
 
 func list_emails(w http.ResponseWriter, r *http.Request) {
-	gservice, err := gmail.New(oauthClient)
+	gservice, err := gmail.New(makeClient(r))
 	if err != nil {
 		log.Fatalf("Failed to create new gmail service => %s", err.Error())
 	}
@@ -180,10 +80,15 @@ func randomString(length int) (str string) {
 	return base64.StdEncoding.EncodeToString(b)
 }
 
+func hi(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `<h1>hi</h1><a href="/list">list emails</a>`)
+}
+
 func main() {
-	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/", hi)
 	http.HandleFunc("/authorize", handleAuthorize)
-	http.HandleFunc("/list", list_emails)
+	http.HandleFunc("/authenticate", needAuth)
+	http.Handle("/list", checkIfAuthenticated(http.HandlerFunc(list_emails)))
 
 	//Google will redirect to this page to return your code, so handle it appropriately
 	http.HandleFunc("/oauth2callback", handleOAuth2Callback)
--- oauth.go
+++ oauth.go
@@ -12,49 +12,31 @@ import (
 )
 
 var notAuthenticatedTemplate = template.Must(template.New("").Parse(`
-<html><body>
-You have currently not given permissions to access your data. Please authenticate this app with the Google OAuth provider.
-<form action="/authorize" method="POST"><input type="submit" value="Ok, authorize this app with my id"/></form>
-</body></html>
-`))
-
-var userInfoTemplate = template.Must(template.New("").Parse(`
-<html><body>
-This app is now authenticated to access your Google user info.  Your details are:<br />
-{{.}}
-</body></html>
+<html>
+  <body>
+    You have currently not given permissions to access your data. Please authenticate this app with the Google OAuth provider.
+    <form action="/authorize" method="POST"><input type="submit" value="Ok, authorize this app with my id"/></form>
+  </body>
+</html>
 `))
 
 var oauthCfg = &oauth.Config{
 	ClientId:     clientID,
 	ClientSecret: clientSecret,
-
 	// For Google's oauth2 authentication, use this defined URL
 	AuthURL: "https://accounts.google.com/o/oauth2/auth",
-
 	// For Google's oauth2 authentication, use this defined URL
 	TokenURL: "https://accounts.google.com/o/oauth2/token",
-
 	// To return your oauth2 code, Google will redirect the browser to this page that you have defined
 	// TODO: This exact URL should also be added in your Google API console for this project
 	// within "API Access"->"Redirect URIs"
 	RedirectURL: "http://localhost:5000/oauth2callback",
-
 	// This is the 'scope' of the data that you are asking the user's permission to access.
 	// For getting user's info, this is the url that Google has defined.
 	Scope: gmail.MailGoogleComScope,
 }
 
-// This is the URL that Google has defined so that an authenticated application may
-// obtain the user's info in json format
-const profileInfoURL = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
-
-func checkIfAuthenticated(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, sessionKey)
-	log.Println(session.ID)
-}
-
-func handleRoot(w http.ResponseWriter, r *http.Request) {
+func needAuth(w http.ResponseWriter, r *http.Request) {
 	notAuthenticatedTemplate.Execute(w, nil)
 }
 
@@ -93,4 +75,21 @@ func handleOAuth2Callback(w http.ResponseWriter, r *http.Request) {
 	for _, m := range resp.Messages {
 		fmt.Fprintf(w, m.Id+"<br>")
 	}
+}
+
+// makeClient creates an oauth2 client from a session variable
+func makeClient(r *http.Request) *http.Client {
+	t := &oauth.Transport{
+		Config: oauthCfg,
+	}
+
+	session, err := store.Get(r, sessionKey)
+	if err != nil {
+		return nil
+	}
+
+	// Exchange the received code for a token
+	t.Exchange(session.Values[codeKey].(string))
+
+	return t.Client()
 }
